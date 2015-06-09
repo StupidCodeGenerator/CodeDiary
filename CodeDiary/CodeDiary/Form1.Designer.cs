d7395c1a02c46df638b18ae72e5e197ed4cfdf2b
StupidCodeGenerator <13609817879@163.com>
2015-06-09 16:45:01 +0800

    First update

---
+++ CodeDiary/CodeDiary/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CodeDiary {
+    partial class FormMain {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxHistory = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBoxHistory
+            // 
+            this.textBoxHistory.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxHistory.Location = new System.Drawing.Point(12, 12);
+            this.textBoxHistory.Multiline = true;
+            this.textBoxHistory.Name = "textBoxHistory";
+            this.textBoxHistory.Size = new System.Drawing.Size(761, 372);
+            this.textBoxHistory.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInput.Location = new System.Drawing.Point(12, 390);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(613, 129);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpen.Location = new System.Drawing.Point(631, 390);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(142, 53);
+            this.buttonOpen.TabIndex = 2;
+            this.buttonOpen.Text = "OPEN";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSubmit.Location = new System.Drawing.Point(631, 449);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(142, 70);
+            this.buttonSubmit.TabIndex = 4;
+            this.buttonSubmit.Text = "SUBMIT";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 531);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.textBoxHistory);
+            this.Name = "FormMain";
+            this.Text = "CodeDiary";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxHistory;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ CodeDiary/CodeDiary/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeDiary {
+    public partial class FormMain : Form {
+
+        string fileName = null;
+
+        public FormMain() {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e) {
+            textBoxInput.KeyUp += textBoxInput_KeyUp;
+        }
+
+        void textBoxInput_KeyUp(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter && e.Modifiers == Keys.Control) {
+                Submit();
+                textBoxInput.Clear();
+            }
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e) {
+            Submit();
+        }
+
+        private void Submit() {
+            if (!String.IsNullOrEmpty(textBoxInput.Text)) {
+                string newContent = "--" + DateTime.Now.ToString() + "--\r\n";
+                newContent += textBoxInput.Text + "\r\n";
+                textBoxHistory.AppendText(newContent);
+
+                if (!string.IsNullOrEmpty(fileName)) {
+                    System.IO.File.WriteAllText(fileName, textBoxHistory.Text);
+                } else {
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK) {
+                        this.fileName = saveFileDialog.FileName;
+                        System.IO.File.WriteAllText(saveFileDialog.FileName, textBoxHistory.Text);
+                    }
+                }
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e) {
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                textBoxHistory.Text = System.IO.File.ReadAllText(openFileDialog.FileName);
+                this.fileName = openFileDialog.FileName;
+            }
+        }
+    }
+}
